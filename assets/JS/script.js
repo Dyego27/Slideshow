edaddfac74fc7f9bebee9f1c55c6005a9e258c45
Dyego27 <dyegoid123@gmail.com>
2024-11-19 08:49:43 -0300

    Fim js,e a responsitividade tambem.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,22 +1,81 @@
-'use strict'
+'use strict';
+
 const images = [
-    {'id':'1','url':'assets/IMG/img.jpg'},
-    {'id':'1','url':'assets/IMG/img3.jpg'},
-    {'id':'1','url':'assets/IMG/img4.jpg'},
-    {'id':'1','url':'assets/IMG/img2.jpg'},
-    {'id':'1','url':'assets/IMG/img5.jpg'},
+    { 'id': '1', 'url':'assets/IMG/img.jpg' },
+    { 'id': '2', 'url':'assets/IMG/img2.jpg' },
+    { 'id': '3', 'url':'assets/IMG/img3.jpg' },
+    { 'id': '4', 'url':'assets/IMG/img4.jpg' },
+    { 'id': '5', 'url':'assets/IMG/img5.jpg' },
 ]
 
-const container = document.querySelector('#container-itens')
+const containerItems = document.querySelector('#container-items');
+const containerIndicators = document.querySelector('.indicators');
 
-const loadImages = (images, container) => {
-    images.forEach(image => {
-        container.innerHTML += `
-        <div class='item'>
-            <img src='${image.url}'
-        </div>
-        `
-    });
+const createIndicators = (images, container) => {
+    images.forEach ( image => {
+        container.innerHTML += `<span data-number=${image.id}>${image.id}</span>`
+    })
 }
 
-loadImages( images, container )
+const loadImages = ( images, container ) => {
+    images.forEach ( image => {
+        container.innerHTML += `
+            <div class='item' data-number=${image.id}>
+                <img src='${image.url}'
+            </div>
+        `
+    })
+}
+
+loadImages( images, containerItems );
+createIndicators(images, containerIndicators);
+
+let items = document.querySelectorAll('.item');
+
+const removeClassSelected = () => {
+    const indicators = document.querySelectorAll('span');
+    indicators.forEach( indicator => indicator.classList.remove ('selected'));
+} 
+
+const selectIndicator = (number) => {
+    removeClassSelected();
+    const indicator = document.querySelector(`span[data-number="${number}"]`)
+    indicator.classList.add('selected')
+} 
+
+const previous = () => {
+    containerItems.appendChild(items[0]);
+    items = document.querySelectorAll('.item');
+    selectIndicator (items[1].dataset.number)
+}
+
+const next = () => {
+    const lastItem = items[items.length - 1];
+    containerItems.insertBefore( lastItem, items[0] );
+    items = document.querySelectorAll('.item');
+    selectIndicator (items[1].dataset.number)
+}
+
+
+
+
+const clickIndicators = ({target}) => {
+
+    if ( target.tagName == 'SPAN') {
+        const selectedIndicator = target.dataset.number;
+        let visibleSlide = items[1].dataset.number
+        if (selectedIndicator !== visibleSlide){
+            
+            const autoNext = setInterval ( () => {
+                document.querySelector('#next').click();
+                visibleSlide = items[1].dataset.number;
+                if (selectedIndicator == visibleSlide) clearInterval(autoNext);
+            }, 100);
+        }
+    }
+
+}
+
+document.querySelector('#previous').addEventListener('click', previous);
+document.querySelector('#next').addEventListener('click', next);
+document.getElementById('indicators').addEventListener('click',clickIndicators);
